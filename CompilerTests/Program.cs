53d431b2f9d1b60a267f035bcc05175c9b1641ce
Marcus Aurelius <Marcus.Aurelius@gallimathias.de>
2017-10-21 19:56:57 +0200

    #1 Implement NewLines and more
    Add more regex expression to tokendefinition "space"

--- CompilerTests/Program.cs
+++ CompilerTests/Program.cs
@@ -26,5 +26,13 @@ namespace CompilerTests
             Assert.AreEqual(compiler.Run(@" ret  1  +  1 ; ")(), 2);
             Assert.AreEqual(compiler.Run(@"var a= 2;ret a+1;")(), 3);
         }
+
+        [TestMethod]
+        public void NewLineTest()
+        {
+            var compiler = new ArrowCompiler();
+            Assert.AreEqual(compiler.Run($"ret {Environment.NewLine}1 + 1;")(), 2);
+            Assert.AreEqual(compiler.Run($"var {Environment.NewLine}a= 2;{Environment.NewLine}ret 1 + 1;")(), 2);
+        }
     }
 }
