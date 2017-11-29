9359cc3f78b7aa644df90a51381a674498acb868
Marcus Aurelius <Marcus.Aurelius@gallimathias.de>
2017-11-29 19:56:12 +0100

    Refactor Tests

---
+++ CompilerTests/MethodTests.cs
@@ -0,0 +1,102 @@
+﻿using Arrow.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompilerTests
+{
+    [TestClass]
+    public class MethodTests
+    {
+        ArrowCompiler compiler;
+
+        public MethodTests()
+        {
+            compiler = new ArrowCompiler();
+        }
+
+        [TestMethod]
+        public void MethodTest0()
+        {
+            var assembly = compiler.GetAssembly(@"
+                def Test : void
+             {
+                }
+                "
+                );
+
+            Assert.AreNotEqual(assembly, null);
+
+        }
+
+        [TestMethod]
+        public void MethodTest1()
+        {
+            var assembly = compiler.GetAssembly(@"
+                def Test : void
+             {
+                    var a = 1 + 1;
+                }
+                "
+                 );
+
+            Assert.AreNotEqual(assembly, null);
+        }
+
+        [TestMethod]
+        public void MethodTest2()
+        {
+            var assembly = compiler.GetAssembly(@"
+                def Test(a : int, b : int) : int
+                {
+                    ret a + b;
+                }
+                "
+               );
+
+            Assert.AreNotEqual(assembly, null);
+        }
+
+        [TestMethod]
+        public void MethodTest3()
+        {
+            var assembly = compiler.GetAssembly(@"
+                def Test() : int
+                {
+                }
+                "
+                           );
+
+            Assert.AreNotEqual(assembly, null);
+        }
+
+        [TestMethod]
+        public void MethodTest4()
+        {
+            var assembly = compiler.GetAssembly(@"
+                def Test : int
+                {
+                }
+                "
+                           );
+
+            Assert.AreNotEqual(assembly, null);
+        }
+
+        [TestMethod]
+        public void MethodTest5()
+        {
+            var assembly = compiler.GetAssembly(@"
+                def Test
+                {
+                }
+                "
+               );
+
+            Assert.AreNotEqual(assembly, null);
+        }
+    }
+}
---
+++ CompilerTests/WhiteSpaceTests.cs
@@ -0,0 +1,51 @@
+﻿using Arrow.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompilerTests
+{
+    [TestClass]
+    public class WhiteSpaceTests
+    {
+        ArrowCompiler compiler;
+
+        public WhiteSpaceTests()
+        {
+            compiler = new ArrowCompiler();
+        }
+
+        [TestMethod]
+        public void WhiteSpaceTest0()
+        {
+            Assert.AreEqual(compiler.RunFunc<int>(@"ret 1+1;")(), 2);
+        }
+
+        [TestMethod]
+        public void WhiteSpaceTest1()
+        {
+            Assert.AreEqual(compiler.RunFunc<int>(@"ret 1 + 1;")(), 2);
+        }
+
+        [TestMethod]
+        public void WhiteSpaceTest2()
+        {
+            Assert.AreEqual(compiler.RunFunc<int>(@" ret  1  +  1 ; ")(), 2);
+        }
+
+        [TestMethod]
+        public void WhiteSpaceTest3()
+        {
+            Assert.AreEqual(compiler.RunFunc<int>(@" ret  1  +  1 ; ")(), 2);
+        }
+
+        [TestMethod]
+        public void WhiteSpaceTest4()
+        {
+            Assert.AreEqual(compiler.RunFunc<int>(@"var a= 2;ret a+1;")(), 3);
+        }
+    }
+}
