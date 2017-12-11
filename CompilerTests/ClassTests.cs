b2fdfec1146dceaf37cac0bb1acca060037fe712
Marcus Aurelius <Marcus.Aurelius@gallimathias.de>
2017-12-11 16:08:47 +0100

    #3 Add Class tests

---
+++ CompilerTests/ClassTests.cs
@@ -0,0 +1,55 @@
+﻿using Arrow.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompilerTests
+{
+    [TestClass]
+    public class ClassTests
+    {
+        private ArrowCompiler compiler;
+
+        public ClassTests()
+        {
+            compiler = new ArrowCompiler();
+        }
+
+        [TestMethod]
+        public void EmptyClassTest()
+        {
+            compiler.GetAssembly("class test;");
+        }
+
+        [TestMethod]
+        public void EmptyClassInheritanceTest()
+        {
+            compiler.GetAssembly(
+                "class test0;" +
+                "class test1 < test0;");
+        }
+
+        [TestMethod]
+        public void ClassTest()
+        {
+            compiler.GetAssembly(
+                @"class test {
+
+                  }");
+        }
+
+        [TestMethod]
+        public void ClassInheritanceTest()
+        {
+            compiler.GetAssembly(
+                @"
+                class test0;
+                class test < test0 {
+
+                  }");
+        }
+    }
+}
